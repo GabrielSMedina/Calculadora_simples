cae8b2dc424e3d39f044f97b648711c5b8b2d277
Gabriel Silva Medina <42703913+GabrielSMedina@users.noreply.github.com>
2022-03-13 14:50:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,93 @@
-import inputs
-import operacoes
+# Funcoes
+# Funcao de soma
+def soma(n1, n2):
+    return n1 + n2
 
-# Criando o dicionario que armazena nossas chamadas de funções
-dic = {'+': operacoes.soma,
-       '-': operacoes.subtracao,
-       '*': operacoes.multiplicacao,
-       '/': operacoes.divisao,
-       '**': operacoes.exponencial,
-       'r': operacoes.raiz
-       }
 
-# Definindo algumas variaveis
-letra = None
+# Funcao de subtracao
+def subtrai(n1, n2):
+    return n1 - n2
+
+
+# Funcao de multiplicacao
+def multiplica(n1, n2):
+    return n1 * n2
+
+
+# Funcao de divide
+def divide(n1, n2):
+    return n1 / n2
+
+
+# Funcao de raiz
+def raiz(n1, n2):
+    return n1 ** 1 / n2
+
+
+# Funcao de exponencial
+def ex(n1, n2):
+    return n1 ** n2
+
+
+# Funcao de atribuicao de valores para resultado inexistente
+def input_1():
+    n1 = float(input('n1 = '))
+    op = input('operacao = ')
+    n2 = float(input('n2 = '))
+    return n1, op, n2
+
+
+# Funcao de atribuicao de valores para resultado existente
+def input_2():
+    op = input('operacao = ')
+    n2 = float(input('n2 = '))
+    return op, n2
+
+
+# Dicionario
+dicionario = {'+': soma,
+              '-': subtrai,
+              '*': multiplica,
+              '/': divide,
+              'r': raiz,
+              '**': ex}
+
+# Variaveis
+# Variavel que dita se uma opecarao sera feita do zero
 resultado = None
 
-# Mostrando as operações possiveis ao usuario
-print(f'Operações:\n+ = soma\n- = subtração\n* = multiplicação'
-      f'\n/ = divisão\n** = potencia na forma (n1**n2)\nr = n1 ** 1/n2\n\n')
-
-# Iniciando o loop
+# Loop princial
 while True:
-    # Iniciando uma nova operação sem dependencia da operação anterior
-    if letra is None:
-        #inputs
-        n1, op, n2 = inputs.entrada()
-        func = dic[op]
-        resultado = func(n1, n2)
-        print(f"{n1} {op}  {n2} : {resultado}")
-        letra = ''
-    letra = input('Deseja continuar esta operação(y/n)? ')
-    # Continuara a operação com o resultado anterior
-    if letra == 'y':
-        n1 = resultado
-        op, n2 = inputs.entrada_continua(n1)
-        func = dic[op]
-        resultado = func(n1, n2)
-        print(f"{n1} {op}  {n2} : {resultado}")
-    else:
-        letra = input('Deseja fazer uma nova operação(y/n)? ')
-        # Finaliza o programa
-        if letra == 'n':
-            print('--Encerrando Calculadora--')
-            break
-        # Reinicia a calcularora
+    # Checa se existe um resultado previo
+    if resultado is None:
+        # Pega informacoes da operacao e os numeros
+        n1, op, n2 = input_1()
+        # Verifica se a operacao esta presente no dicionario
+        if op in dicionario:
+            funcao = dicionario[op]
+            # Chama funcao
+            resultado = funcao(n1, n2)
+            print(f'{n1} {op} {n2} = {resultado}')
         else:
-            letra = None
+            print('Operacao invalida!')
+    else:
+        n1 = resultado
+        print(f'n1 = {n1}')
+        # Pega informacoes da operacao e os numeros
+        op, n2 = input_2()
+        # Verifica se a operacao esta presente no dicionario
+        if op in dicionario:
+            funcao = dicionario[op]
+            # Chama funcao
+            resultado = funcao(n1, n2)
+            print(f'{n1} {op} {n2} = {resultado}')
+        else:
+            print('Operacao invalida!')
+
+    pergunta = input('Deseja continuar esta operacao(y/n): ')
+    if pergunta == 'n':
+        resultado = None
+        pergunta_2 = input('Deseja fazer uma nova operacao(y/n): ')
+
+        if pergunta_2 == 'n':
+            break
